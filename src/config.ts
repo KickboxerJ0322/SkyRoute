b86269c3649822d4e92a4db959a339a27a003fed
Kickboxer J <164498916+KickboxerJ0322@users.noreply.github.com>
2026-09-26 10:57:00 +0900

    Move SkyRoute startup position

--- src/config.ts
+++ src/config.ts
@@ -27,8 +27,8 @@ export const HANEDA_AIRPORT = {
 // Initial Map & Camera Settings
 export const INITIAL_MAP_CONFIG = {
   center: {
-    lat: 35.5422398,
-    lng: 139.7841925,
+    lat: 35.5447199,
+    lng: 139.7799157,
     altitude: 10,
   },
   // Low, close establishing shot of Haneda with the parked 787 visible on the apron.
